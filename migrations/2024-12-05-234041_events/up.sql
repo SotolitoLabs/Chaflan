035c4a97c76018a6bdb511774e5a42d3c4707aca
Iván Chavero <ivan.chavero@ugd.gob.mx>
2024-12-09 09:47:12 -0600

    Added database fields

--- migrations/2024-12-05-234041_events/up.sql
+++ migrations/2024-12-05-234041_events/up.sql
@@ -1,10 +1,12 @@
 -- Your SQL goes here
 
-CREATE TABLE IF NOT EXISTS Event (
+CREATE TABLE IF NOT EXISTS Events (
     Id UUID,
-    Name TEXT,
-    Venue TEXT,
+    Name TEXT NOT NULL,
+    Venue TEXT NOT NULL,
     Address TEXT,
+    Image VARCHAR(255),
+    Comments TEXT,
     ContactName VARCHAR(255),
     Starts_at TIMESTAMP NOT NULL DEFAULT NOW(),
     Ends_at TIMESTAMP NOT NULL DEFAULT NOW(),
