6cc2d96b2f944396d43155b2529b5afd513a1851
Iván Chavero <ichavero@chavero.com.mx>
2025-03-02 16:37:38 -0600

    Generated model for user migration

--- src/models.rs
+++ src/models.rs
@@ -23,3 +23,18 @@ pub struct Event {
     pub ends_at: NaiveDateTime,
 }
 
+#[derive(Clone, Debug, Identifiable, Queryable, QueryableByName, Insertable, Serialize, Deserialize)]
+#[diesel(table_name = users)]
+pub struct User {
+    pub id: Uuid,
+    pub name: Option<String>,
+    pub email: String,
+    pub password: Option<String>,
+    pub oauth_provider: String,
+    pub oauth_user_id: String,
+    pub access_token: String,
+    pub refresh_token: Option<String>,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
+
--- src/schema.rs
+++ src/schema.rs
@@ -15,3 +15,23 @@ diesel::table! {
         ends_at -> Timestamp,
     }
 }
+
+diesel::table! {
+    users (id) {
+        id -> Uuid,
+        name -> Nullable<Text>,
+        email -> Text,
+        password -> Nullable<Text>,
+        oauth_provider -> Text,
+        oauth_user_id -> Text,
+        access_token -> Text,
+        refresh_token -> Nullable<Text>,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    events,
+    users,
+);
